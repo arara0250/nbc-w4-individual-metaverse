d1ff4047138c7ae9c27bc3e5a7f4a4b10a762973
arara0250 <arara4281@gmail.com>
2025-10-26 18:29:33 +0900

    feat : Implement player movement feature

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    
+
+    private Vector2 movingDir = Vector2.zero;
+    public Vector2 MovingDir { get => movingDir; }
+
+
+    private Vector2 lookDir = Vector2.zero;
+    public Vector2 LookDir { get => lookDir; }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float movingX = Input.GetAxisRaw("Horizontal");
+        float movingY = Input.GetAxisRaw("Vertical");
+
+        movingDir = new Vector2(movingX, movingY).normalized;
+        _rigidbody.velocity = movingDir * 5f;
+    }
+}
