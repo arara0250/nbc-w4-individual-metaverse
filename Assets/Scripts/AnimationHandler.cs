8091172fc5fc516c3643cd4c6cce711da97d956e
arara0250 <arara4281@gmail.com>
2025-10-27 11:20:13 +0900

    feat : 플레이어 이동 방향별 애니메이션 전환 로직 작성

--- Assets/Scripts/AnimationHandler.cs
+++ Assets/Scripts/AnimationHandler.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class AnimationHandler : MonoBehaviour
 {
     private static readonly int IsMoving = Animator.StringToHash("IsMoving");
-    
+    private static readonly int MoveFront = Animator.StringToHash("MoveFront");
+    private static readonly int MoveSide = Animator.StringToHash("MoveSide");
+    private static readonly int MoveBack = Animator.StringToHash("MoveBack");
+
+
     private Animator animator;
+    private SpriteRenderer playerRenderer;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        playerRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void Move(Vector2 obj)
@@ -21,6 +27,39 @@ public class AnimationHandler : MonoBehaviour
             return;
         }
 
+        if (obj.x < -0.5f || obj.x > 0.5f)
+        {
+            // 좌우 이동 (대각선 이동 포함)
+            animator.SetBool(MoveFront, false);
+            animator.SetBool(MoveSide, true);
+            animator.SetBool(MoveBack, false);
+        }
+        else if (obj.y == 1)
+        {
+            // 뒤 (위쪽 방향) 로 이동
+            animator.SetBool(MoveFront, false);
+            animator.SetBool(MoveSide, false);
+            animator.SetBool(MoveBack, true);
+        }
+        else
+        {
+            animator.SetBool(MoveFront, true);
+            animator.SetBool(MoveSide, false);
+            animator.SetBool(MoveBack, false);
+        }
+
         animator.SetBool(IsMoving, obj.magnitude > 0.5f);
+    }
+
+    public void Rotate(Vector2 moveDir)
+    {
+        if (moveDir.x < 0.5f)
+        {
+            playerRenderer.flipX = true;
+        }
+        else if (moveDir.x > 0.5f)
+        {
+            playerRenderer.flipX = false;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -23,12 +24,39 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        HandleMovement();
+        animationHandler.Rotate(movingDir);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = movingDir * 5f;
+    }
+
+    private void HandleMovement()
+    {
         float movingX = Input.GetAxisRaw("Horizontal");
         float movingY = Input.GetAxisRaw("Vertical");
 
+        // 디버깅 코드
+        string input = "";
+        if (movingY == 1) input += "↑";
+        else if (movingY == -1) input += "↓";
+
+        if (movingX == 1) input += "→";
+        else if (movingX == -1) input += "←";
+
+        Debug.Log($"{input}");
+        input = "";
+
+
         movingDir = new Vector2(movingX, movingY).normalized;
-        _rigidbody.velocity = movingDir * 5f;
 
         animationHandler.Move(movingDir);
+    }
+
+    private void HandleInteraction()
+    {
+        // TODO : Player 와 Object 상호작용 구현
     }
 }
