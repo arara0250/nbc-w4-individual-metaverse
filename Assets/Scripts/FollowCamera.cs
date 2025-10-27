866c8c0c525a383b00afcf8226583eb69bbd7e9e
arara0250 <arara4281@gmail.com>
2025-10-27 14:12:47 +0900

    feat : 카메라 - 플레이어 추적 기능 추가

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;    // Player
+    private Vector3 offset;     // Player 와 Camera 사이의 거리
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target == null) return;     // target (Player) 가 Inspector 상에서 연결되지 않았을 경우
+
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+
+        Vector3 cameraPos = transform.position;
+        cameraPos = target.position + offset;
+        transform.position = cameraPos;
+    }
+}
